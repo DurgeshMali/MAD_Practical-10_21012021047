77bd6626808f05f48845c74b44a4a844b73408a2
Durgesh <durgeshmali5824@gmail.com>
2023-10-20 14:44:22 +0530

    pr_10

--- app/src/main/java/com/example/mad_practical_10_21012021047/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012021047/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.mad_practical_10_21012021047
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var personListView:ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        personListView = findViewById(R.id.listview1)
+
+        val btn:FloatingActionButton = findViewById(R.id.floating_button_1)
+        btn.setOnClickListener{
+            setPersondata_to_Listview()
+        }
+    }
+    fun setPersondata_to_Listview(){
+        personListView.adapter=PersonAdapter(this, arrayListOf(
+            Person("1","Durgesh","9332493494","durgesh@gnu.ac.in","uma hostel",24.67,40.34),
+            Person("2","Yash","3427935923","yash@gnu.ac.in","h hostel",45.90,43.89),
+            Person("3","Jainish","9873456732","jainish@gnu.ac.in","h hostel",34.34,23.56),
+            Person("4","Bharat","4325692845","bharat@gnu.ac.in","uma hostel",23.56,24.67),
+            Person("5","Harsh","4325692845","harsh@gnu.ac.in","g hostel",23.56,24.67)
+        ))
     }
 }
--- app/src/main/java/com/example/mad_practical_10_21012021047/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012021047/PersonAdapter.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class PersonAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item,parent,false)
+        val persondata = array.get(position)
+        view.findViewById<TextView>(R.id.textview1).text = persondata.name
+        view.findViewById<TextView>(R.id.textview2).text = persondata.phoneNo
+        view.findViewById<TextView>(R.id.textview3).text = persondata.emailid
+        view.findViewById<TextView>(R.id.textview4).text = persondata.address
         return view
     }
 }
