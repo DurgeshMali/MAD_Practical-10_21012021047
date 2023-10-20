81c921646dda015fb06fff6481de4ac3daa328f5
student <student@gnu.ac.in>
2023-10-20 12:34:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_10_21012021047/Person.kt
@@ -0,0 +1,14 @@
+package com.example.mad_practical_10_21012021047
+
+import java.io.Serializable
+
+class Person(val id:String,
+             val name: String,
+             val emailid: String,
+             val phoneNo: String,
+             val address: String,
+             val latitude: Double,
+             val longitude: Double):Serializable{
+
+
+}
